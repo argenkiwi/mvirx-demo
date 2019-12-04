f4c50dc023f7789e6376cf018a4d9b66cbf72f54
Leandro Peralta <leandro.peralta@trademe.co.nz>
2019-12-04 16:44:39 +1300

    Extract main model class.

--- app/src/main/java/nz/co/trademe/demo/MainModel.kt
+++ app/src/main/java/nz/co/trademe/demo/MainModel.kt
@@ -1,5 +1,11 @@
 package nz.co.trademe.demo
 
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.processors.BehaviorProcessor
+import io.reactivex.processors.FlowableProcessor
+import io.reactivex.processors.PublishProcessor
+import java.util.concurrent.TimeUnit
 import kotlin.math.max
 
 sealed class MainEvent {
@@ -12,4 +18,20 @@ data class MainState(val time: Int) {
         is MainEvent.Increment -> copy(time = time + 1)
         is MainEvent.Decrement -> copy(time = max(0, time - 1))
     }
+}
+
+class MainModel {
+
+    val events: FlowableProcessor<MainEvent> = PublishProcessor.create()
+    val state: FlowableProcessor<MainState> = BehaviorProcessor.create()
+
+    fun subscribe() = CompositeDisposable(
+        events
+            .scan(MainState(10), { state, event -> state.reduce(event) })
+            .subscribe { state.onNext(it) },
+        Observable
+            .timer(1000, TimeUnit.MILLISECONDS)
+            .repeat()
+            .subscribe { events.onNext(MainEvent.Decrement) }
+    )
 }
--- app/src/main/java/nz/co/trademe/demo/MainViewModel.kt
+++ app/src/main/java/nz/co/trademe/demo/MainViewModel.kt
@@ -3,29 +3,14 @@ package nz.co.trademe.demo
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
-import io.reactivex.Observable
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.processors.BehaviorProcessor
-import io.reactivex.processors.FlowableProcessor
-import io.reactivex.processors.PublishProcessor
-import java.util.concurrent.TimeUnit
 
 class MainViewModel : ViewModel() {
 
-    private val events: FlowableProcessor<MainEvent> = PublishProcessor.create()
-    private val state: FlowableProcessor<MainState> = BehaviorProcessor.create()
+    private val model = MainModel()
 
-    val liveState: LiveData<MainState> = LiveDataReactiveStreams.fromPublisher(state)
+    val liveState: LiveData<MainState> = LiveDataReactiveStreams.fromPublisher(model.state)
 
-    private val disposable = CompositeDisposable(
-        events
-            .scan(MainState(10), { state, event -> state.reduce(event) })
-            .subscribe { state.onNext(it) },
-        Observable
-            .timer(1000, TimeUnit.MILLISECONDS)
-            .repeat()
-            .subscribe { decrement() }
-    )
+    private val disposable = model.subscribe()
 
     override fun onCleared() {
         super.onCleared()
@@ -33,10 +18,6 @@ class MainViewModel : ViewModel() {
     }
 
     fun increment() {
-        events.onNext(MainEvent.Increment)
-    }
-
-    private fun decrement() {
-        events.onNext(MainEvent.Decrement)
+        model.events.onNext(MainEvent.Increment)
     }
 }
